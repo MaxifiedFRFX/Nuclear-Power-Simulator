f98b05a87a3aeb2d8e21465352355b3a3dc7263c
CaedenScott <114004469+CaedenScott@users.noreply.github.com>
2022-12-06 10:49:43 -0500

    front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect, useRef } from 'react'
+import { Button } from '@mui/material'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const ctx = canvasRef.current
+
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        animation: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    })
+
+    return () => {
+      myChart.destroy()
+    }
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="powerPlant">
+        <section className="dashboard">
+          <div className="plantName">
+            <button>Change Name</button>
+            <h1>Power Plant</h1>
+          </div>
+          <div className="graphAllReactors">
+            <div>
+              <canvas ref={canvasRef}></canvas>
+            </div>
+          </div>
+          <div className="totalOutput">
+            <h2>Average Temperature: 1234</h2>
+            <h2>Total Output: 1234 GW</h2>
+          </div>
+          <div className="systemLogs"></div>
+        </section>
+        <section className="reactorsDisplay">
+          <h1>Reactors</h1>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <footer>
+        <section className="dashboardFooter">
+          <Button variant="contained">Enable Coolants on all Reactors</Button>
+          <Button variant="contained">Disable Coolants on all Reactors</Button>
+          <Button variant="contained">Global Controlled Shutdown</Button>
+          <Button variant="contained" color="error">Global Reset</Button>
+        </section>
+        <section className="reactorsFooter">
+          <Button variant="contained" color="error">Global Emergency Shutdown</Button>
+        </section>
+      </footer>
+    </>
   )
 }
 
